07082311590842bc0624c10f154b3a5a850f8f30
Qing Jiao <jiaoqingsunny@gmail.com>
2017-04-17 18:32:47 +0800

    add WaterView

--- Explode/Explode/MainViewController.swift
+++ Explode/Explode/MainViewController.swift
@@ -10,6 +10,7 @@ import SnapKit
 
 class MainViewController: UIViewController {
   let gameView = GameView()
+  var waterView: WaterView?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,7 +22,22 @@ class MainViewController: UIViewController {
       make.top.left.width.height.equalTo(self.view)
     }
     
+    let bgIV2 = UIImageView()
+    bgIV2.image = UIImage(named: "BG_splash")
+    bgIV2.contentMode = .scaleAspectFit
+    view.addSubview(bgIV2)
+    bgIV2.snp.makeConstraints{ make in
+      make.left.width.bottom.equalTo(self.view)
+      make.height.equalTo(300)
+    }
+    
     view.addSubview(gameView)
+    
+    waterView = WaterView(frame: CGRect(x: 0, y: 400, width: view.frame.size.width, height: 100))
+    waterView!.fill(to: 1)
+    waterView!.fillColor = UIColor.yellow
+    view.addSubview(waterView!)
+    waterView!.startAnimation()
   }
   
   override func viewWillLayoutSubviews() {
@@ -33,5 +49,9 @@ class MainViewController: UIViewController {
     gameFrame.size.width = view.frame.size.width
     gameFrame.size.height = view.frame.size.width
     gameView.frame = gameFrame
+    
+    if let wv = waterView {
+      wv.frame.origin.y = self.view.frame.size.height - wv.frame.size.height
+    }
   }
 }
---
+++ Explode/Explode/WaterView.swift
@@ -0,0 +1,23 @@
+//
+//  WaterView.swift
+//  Explode
+//
+//  Created by Qing Jiao on 17/4/17.
+//  Copyright © 2017 Qing Jiao. All rights reserved.
+//
+
+import UIKit
+import BAFluidView
+
+class WaterView: BAFluidView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
